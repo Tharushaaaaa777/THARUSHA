2b514288cf87957f333ee4881126695904867570
Tharushaaaaa777 <tharushasandipa250@gmail.com>
2025-06-23 17:20:37 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
+module.exports = {    
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=mrgykY6S#BJE7ot5LCUJ__UiNCMnbADEZOomEpvD6ifraXrAMFZQ',
+OWNER_NUMBER: process.env.OWNER_NUMBER || "94718913389",
+ALIVE: process.env.ALIVE || `default`,
+OWNER_NAME: process.env.OWNER_NAME || 'Sahas' ,     
+POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'postgres://izumimd_meje_user:0Vhm5vKGZ7ORt2FlJBQf4d6EtRdeuE8z@dpg-cn0o2imn7f5s73fa46q0-a.frankfurt-postgres.render.com/izumimd_meje',
+PREFIX:  process.env.PREFIX || ['.'] ,
+FOOTER: '> BOT CREATED BY SHADOW MOVIE X KILAR',
+DIRECTION: true,
+IMAGE: process.env.IMAGE || `https://i.ibb.co/4rKv8p3/9046.jpg`,
+LOGO: process.env.LOGO || `https://i.ibb.co/4rKv8p3/9046.jpg`     
+};
