612448515e9194933da8f1a00399a59e358b1d0f
Tharushaaaaa777 <tharushasandipa250@gmail.com>
2025-06-26 08:40:29 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,15 +5,15 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {    
-SESSION_ID: process.env.SESSION_ID || 'THARUSHA-MD=6hJBFAAR#XPuv1O0l5fQN__eRM3bAU_SKGUzxcqtRJ4ay7KKJ3p0',
+SESSION_ID: process.env.SESSION_ID || 'THARUSHA-MD=bogTBZ4L#J7foWnodGXBxn_jkZZ7ogSZ6QufespUvwdv4mCj6wzc',
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94740326138", // 👈 do not change this envairement
 ALIVE: process.env.ALIVE || `default`,
-OWNER_NAME: process.env.OWNER_NAME || 'Sahas' ,     
+OWNER_NAME: process.env.OWNER_NAME || 'Tharuzz' ,     
 POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'postgres://izumimd_meje_user:0Vhm5vKGZ7ORt2FlJBQf4d6EtRdeuE8z@dpg-cn0o2imn7f5s73fa46q0-a.frankfurt-postgres.render.com/izumimd_meje',
 PREFIX:  process.env.PREFIX || ['.'] ,
-FOOTER: '> BOT CREATED BY SHADOW MOVIE X KILAR',
+FOOTER: '> BOT CREATED BY THARUZZ OFC',
 DIRECTION: true,
-IMAGE: process.env.IMAGE || `https://i.ibb.co/4rKv8p3/9046.jpg`,
-LOGO: process.env.LOGO || `https://i.ibb.co/4rKv8p3/9046.jpg`,
+IMAGE: process.env.IMAGE || `https://i.ibb.co/M5cQsgwj/Tharusha-Md.jpg`,
+LOGO: process.env.LOGO || `https://i.ibb.co/M5cQsgwj/Tharusha-Md.jpg`,
 // MODE: process.env.MODE || `button`
 };
