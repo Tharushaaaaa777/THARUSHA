f708885adb635de6d609d40c9dcc0b1703c0e744
Tharushaaaaa777 <tharushasandipa250@gmail.com>
2025-06-27 05:58:20 +0530

    Create system.js

---
+++ plugins/system.js
@@ -0,0 +1,35 @@
+//const config = require('../config')
+const {cmd , commands} = require('../lib/command')
+const os = require("os")
+const {runtime} = require('../lib/functions')
+const tharuzz_md_logo = "https://i.ibb.co/M5cQsgwj/Tharusha-Md.jpg"
+
+cmd({
+    pattern: "system",
+    react: "♠️",
+    alias: ["uptime","status","runtime"],
+    desc: "cheack uptime",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let status = `*╭──────────●●►*
+*THARUZZ MD UPTIME LIST↷*
+
+*_UPTIME:➠_*  ${runtime(process.uptime())}
+
+*_RAM USAGE:➠_* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+
+*_HOSTNAME:➠_* ${os.hostname()}
+
+*_OWNER:➠_* *Tharuzz ofc*
+*╰──────────●●►*
+`
+await conn.sendMessage(from,{image:{url:tharuzz_md_logo},caption:`${status}`},{quoted:mek})
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
