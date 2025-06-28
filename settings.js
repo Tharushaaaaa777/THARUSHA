ed2c781b957d714b1c4c97387f1164f9480df104
Tharushaaaaa777 <tharushasandipa250@gmail.com>
2025-06-28 12:28:25 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,14 +4,15 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 
-module.exports = {    
-SESSION_ID: process.env.SESSION_ID || 'THARUSHA-MD=bogTBZ4L#J7foWnodGXBxn_jkZZ7ogSZ6QufespUvwdv4mCj6wzc',
+module.exports = { 
+SESSION_ID: process.env.SESSION_ID || 'THARUSHA-MD=bogTBZ4L#J7foWnodGXBxn_jkZZ7ogSZ6QufespUvwdv4mCj6wzc',  
+AUTO_STATUS_READ:process.env.AUTO_STATUS_READ || 'true',
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94740326138", // 👈 do not change this envairement
 ALIVE: process.env.ALIVE || `default`,
 OWNER_NAME: process.env.OWNER_NAME || 'Tharuzz' ,     
 POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'postgres://izumimd_meje_user:0Vhm5vKGZ7ORt2FlJBQf4d6EtRdeuE8z@dpg-cn0o2imn7f5s73fa46q0-a.frankfurt-postgres.render.com/izumimd_meje',
 PREFIX:  process.env.PREFIX || ['.'] ,
-FOOTER: '> BOT CREATED BY THARUZZ OFC',
+FOOTER: '© ᴘᴏᴡᴇʀᴅ ʙʏ ᴛʜᴀʀᴜꜱʜᴀ ᴍᴅ',
 DIRECTION: true,
 IMAGE: process.env.IMAGE || `https://i.ibb.co/M5cQsgwj/Tharusha-Md.jpg`,
 LOGO: process.env.LOGO || `https://i.ibb.co/M5cQsgwj/Tharusha-Md.jpg`,
