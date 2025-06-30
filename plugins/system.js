0617db4cf1a30e3938179756200a4fc8f452ac73
Tharushaaaaa777 <tharushasandipa250@gmail.com>
2025-06-30 07:13:05 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -3,6 +3,7 @@ const {cmd , commands} = require('../lib/command')
 const os = require("os")
 const {runtime} = require('../lib/functions')
 const tharuzz_md_logo = "https://i.ibb.co/M5cQsgwj/Tharusha-Md.jpg"
+const tharuzzfooter = "*© ᴘᴏᴡᴇʀᴅ ʙʏ ᴛʜᴀʀᴜꜱʜᴀ ᴍᴅ*"
 const pakaya = "`"
 cmd({
     pattern: "system",
@@ -14,13 +15,16 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-let status = `*┏━━━━━━━━━━━━━━━━━━━┓*
+let status = `*${pakaya}THARUSHA-MD SYSTEM INFO . . . 🧬${pakaya}*
+
+*┏━━━━━━━━━━━━━━━━━━━┓*
 *┃ 🚀 ${pakaya}ᴜᴘᴛɪᴍᴇ:${pakaya}*  ${runtime(process.uptime())}
 *┃ 🧬 ${pakaya}ʀᴀᴍ ᴜꜱᴀɢᴇ:${pakaya}* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
 *┃ 🌀 ${pakaya}ʜᴏꜱᴛɴᴀɴᴇ:${pakaya}* ${os.hostname()}
 *┃ 👤 ${pakaya}ᴏᴡɴᴇʀ:${pakaya}* *ᴛʜᴀʀᴜᴢᴢ ᴏꜰᴄ*
 *┗━━━━━━━━━━━━━━━━━━━┛*
-`
+
+> ${tharuzzfooter}`
 await conn.sendMessage(from,{image:{url:tharuzz_md_logo},caption:`${status}`},{quoted:mek})
 
 }catch(e){
